8ee9be4cad9f671ceeb47028e9ef94b56af2c93d
jecaro <jeancharles.quillet@gmail.com>
2023-09-19 09:41:41 +0200

    wip

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          buildInputs = [
+            pkgs.gnumake
+            pkgs.texlive.combined.scheme-full
+          ];
+          # Change the prompt to show that you are in a devShell
+          shellHook = "export PS1='\\[\\e[1;34m\\]dev > \\[\\e[0m\\]'";
+        };
+    };
+}
