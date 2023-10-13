41c09aa35bab8438e0847e04269a9793a7d59f45
jecaro <jeancharles.quillet@gmail.com>
2023-10-13 21:58:14 +0200

    flakify the project

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        name = "jeancharles-quillet-cv";
+        src = ./.;
+
+        buildInputs = [
+          pkgs.gnumake
+          pkgs.texlive.combined.scheme-full
+        ];
+
+        buildPhase = ''
+          make clean all
+        '';
+
+        installPhase= ''
+          mkdir $out
+          cp resume-fr/jeancharles.quillet-fr.pdf $out
+          cp resume-en/jeancharles.quillet-en.pdf $out
+          '';
+      };
+    };
+}
--- shell.nix
+++
@@ -1,4 +0,0 @@
-let pkgs = import <nixpkgs> {};
-in pkgs.mkShell {
-  buildInputs = [ pkgs.gnumake pkgs.texlive.combined.scheme-full ];
-}
