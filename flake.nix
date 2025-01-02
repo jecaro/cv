405c09880262abf43b7136f405b4da052215f438
jecaro <jeancharles.quillet@gmail.com>
2025-01-02 12:20:01 +0100

    Bump to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   outputs = { nixpkgs, ... }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
